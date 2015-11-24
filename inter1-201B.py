aba21a3f4b4d3c17c35c4cff67f9c54ae9e86428
mishakuzma <mishakuzma@gmail.com>
2015-11-24 16:01:16 -0500

    201B.py Changed - Kaprekar Number
    I misunderstood what a Kaprekar number was. A Kaprekar number is a
    number where you take a number, square it, then take the right digits
    and the left digits (if it's odd, left is n-1 and right is n) and add
    the sum of these two sides. It is NOT adding each individual digit, as I
    originally believed. The program has been changed to reflect this.

--- inter1-201B.py
+++ inter1-201B.py
@@ -9,9 +9,29 @@ def main():
 a kaprekar number: "))
 	sqr_num = str(orig_num ** 2)
 	sum_num = 0
+	right = ""
+	left = ""
 
-	for i in sqr_num:
-		sum_num += int(i)
+	for i in range(0, len(sqr_num)):
+		if sqr_num == "1":
+			break
+		elif len(sqr_num) % 2 == 0:
+			if i < (len(sqr_num) / 2):
+				left += sqr_num[i]
+			else:
+				right += sqr_num[i]
+		else:
+			if i+1 <= len(sqr_num) // 2:
+				left += sqr_num[i]
+			else:
+				right += sqr_num[i]
+
+	if sqr_num == "1":
+		sum_num = 1
+	else:
+		right = int(right)
+		left = int(left)
+		sum_num = right + left
 
 	if sum_num == orig_num:
 		print(str(orig_num), "is a kaprekar number.")
