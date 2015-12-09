9f1f44e2dfcf72e6a1bfde68e53284df0f91c2df
mishakuzma <mishakuzma@gmail.com>
2015-12-09 17:39:32 -0500

    234A is complete.
    This program is locked with a user and a pass. If a file does not exist
    to tell these details to the program, a file will be created and details
    will be asked for. If a file does exist, then the user has three
    attempts to open the file with the proper inputs.
    
    I wanted to try out classes again. I had trouble with referencing class
    instances, since I used multiple functions. So, I defined the
    troublesome function as a local function, and referenced the class parts
    within the main function. It works just fine, although I probably didn't
    need to put the function in as a local.
    
    I forgot to move the comments back after I put them outside main, so
    i'll fix that for tomorrow.
    
    In the future, i'd like to make a program that would open the file for
    the user if they forgot the user and pass. I'm thinking that looping
    over a series of letters via ordinal numbers should do the trick.

---
+++ 234A-PassProtect.py
@@ -0,0 +1,65 @@
+import os, time, sys
+
+class Credential:
+	"Holds a username and password, as well as information to\
+ write and read a user and pass."
+
+	def __init__(self, user, password):
+		self.user = user
+		self.password = password
+
+	def writeInfo(self):
+		os.chdir("C:\selfPrograms\dailyProg\outFiles")
+		s = open("passInfo.txt", "w")
+		s.write("User and Pass for 234A: \n")
+		s.write("User: " + str(self.user) + "\n")
+		s.write("Password: " + str(self.password))
+		s.close()
+
+""" existPass() will check a folder for an existing document that 
+contains a username and password. If one exists, the checked user
+and pass will become the one that is checked by the program. If not,
+user will be asked to write the user and pass."""
+def main():
+	def existPass():
+		os.chdir("C:\selfPrograms\dailyProg\outFiles")
+		try:
+			s=open("passInfo.txt", "r")
+			w = s.readline()
+			while w != "":
+				if "User: " in w:
+					user = w[6:w.find("\n")]
+				elif "Password: " in w:
+					passW = w[10:]
+				else:
+					pass
+				w = s.readline()
+			newCred = Credential(user, passW)
+			return newCred
+		except FileNotFoundError:
+			newCred = Credential(str(input("Please input a new user: ")), \
+				str(input("Please input a new pass: ")))
+			newCred.writeInfo()
+			return newCred
+
+	x = 0
+	existPass()
+	while x < 3:
+		userAttempt = str(input("Please enter your Username: "))
+		passAttempt = str(input("Please enter your Password: "))
+
+		if userAttempt == str(existPass().user) and \
+passAttempt == str(existPass().password):
+			break
+		else:
+			x += 1
+			print("You have " + str((3 - x)) + " attempts left.\n")
+
+	if x == 3:
+		print("Access denied")
+		sys.exit()
+	else:
+		print("File opened")
+		sys.exit()
+
+main()
